a2ad11104ffa0a5ed524aa5d3e3f17aa36c49926
Miguelhc95 <miguelhc95@usal.es>
2024-02-24 13:44:43 +0100

    Add exercise2_2 to folder

---
+++ part2/exercise2_2/src/components/Course.jsx
@@ -0,0 +1,15 @@
+const Course = ({ course }) => {
+    const totalExercises = course.parts.reduce((total, part) => total + part.exercises, 0);
+      return (
+        <>
+          <h1>{course.name}</h1>
+          <ul>{course.parts.map(part =>(
+            <li key={part.id}>{part.name}</li>))}
+          </ul>
+          
+          <p><strong>Total exercises: {totalExercises}</strong></p>
+        </>
+      )
+    }
+    
+    export default Course
