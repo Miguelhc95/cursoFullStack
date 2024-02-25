393dda63c7101bbf425a17ccd7ab5521389e641b
Miguelhc95 <miguelhc95@usal.es>
2024-02-25 17:44:21 +0100

    Add exercise2_11 to folder

---
+++ part2/exercise2_11/src/App.jsx
@@ -0,0 +1,111 @@
+import { useState, useEffect } from 'react';
+import Person from './components/Person';
+import axios from 'axios'
+
+const PersonForm = (props) => {
+  const [newName, setNewName] = useState('');
+  const [newPhone, setNewPhone] = useState('');
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    const existingPerson = props.persons.find(person => person.name === newName);
+    const existingPhone = props.persons.find(person => person.phone === newPhone);
+
+    if(existingPerson) {
+      alert(`${newName} is already added to the phonebook`);
+    } else if(existingPhone) {
+      alert(`${newPhone} is already added to the phonebook`);
+    } else {
+      const personObject = {
+        id: props.persons.length + 1,
+        name: newName,
+        phone: newPhone
+      };
+    
+      props.setPersons(props.persons.concat(personObject));
+      setNewName('');
+      setNewPhone(''); 
+    }
+  };
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handlePhoneChange = (event) => {
+    setNewPhone(event.target.value);
+  };
+
+  return(
+    <form onSubmit={addPerson}>
+        name:
+        <input value={newName} onChange={handlePersonChange}/>
+        phone:
+        <input value={newPhone} onChange={handlePhoneChange}/>
+        <button type="submit">add</button>
+      </form>  
+  )
+}
+
+
+const Filter = (props) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const personsToShow = searchTerm === '' 
+    ? props.persons 
+    : props.persons.filter(person => person.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+  return(
+    <div>
+      <form>
+        filter shown with:
+        <input value={searchTerm} onChange={handleSearchChange}/>
+      </form>   
+      <h2>Numbers</h2>
+      <ul>
+        <Persons persons={personsToShow} />
+      </ul>
+    </div>
+  )
+}
+
+
+const Persons = (props) => {
+
+  return(
+    <ul>
+        {props.persons.map(person => 
+          <Person key={person.id} person={person} />
+        )}
+      </ul>
+  )
+} 
+
+const App = () => {
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter persons={persons} />
+      <h2>Add a new</h2>
+      <PersonForm persons={persons} setPersons={setPersons}/>
+    </div>
+  );
+};
+
+export default App;
