7442b916cb0ebf77f9f42354c835e540331cb312
Miguelhc95 <miguelhc95@usal.es>
2024-02-25 13:26:27 +0100

    Add exercise 2_7 to folder

---
+++ part2/exercise2_7/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import Person from './components/Person'
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { id: 1, name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const existingPerson = persons.find(person => person.name === newName)
+
+    if(existingPerson){
+      alert(`${newName} is already added to phonebook`)
+
+    }else{
+      const personObject = {
+        id: persons.length + 1,
+        name: newName
+      }
+    
+      setPersons(persons.concat(personObject))
+      setNewName('')
+    }
+  }
+  
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        name:
+        <input value={newName} onChange={handlePersonChange}/>
+        <button type="submit">add</button>
+      </form>   
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => 
+          <Person key={person.id} person={person} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
