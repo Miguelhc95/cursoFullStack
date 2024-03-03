3a05f1cd94cd235da11900797f364380ff1b3f2e
Miguelhc95 <miguelhc95@usal.es>
2024-03-03 19:31:08 +0100

    Add Exercise3_15 to folder

---
+++ part3/Exercise3_15/index.js
@@ -0,0 +1,99 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan');
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const Person = require('./models/person')
+
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
+
+morgan.token('postData', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body);
+  }
+  return '';
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'));
+
+
+  app.get('/', (request, response) => {
+    Person.find({}).then(persons => {
+      response.json(persons)
+    })
+  })
+  
+  app.get('/api/persons', (request, response) => {
+    Person.find({}).then(persons => {
+      response.json(persons)
+    })
+  })
+
+  app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+  })
+  
+  app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body.name || !body.number) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+
+    const person = new Person({
+      name: body.name,
+      number: body.number,
+    })
+  
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    }).catch(error => {
+      console.log(error)
+      response.status(500).end()
+    })
+  })
+
+  app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+  })
+
+  app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+  
+    const person = new Person({
+      name: body.name,
+      number: body.number,
+    })
+  
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
+  })
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
