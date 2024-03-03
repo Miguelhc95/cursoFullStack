b32cda9d2b196f248ed505d31bda17eb78a2cd4e
Miguelhc95 <miguelhc95@usal.es>
2024-03-03 18:32:44 +0100

    Add Exercise3_13 to folder

---
+++ part3/Exercise3_13/README.md
@@ -0,0 +1,2 @@
+ 
+https://cursofullstack.onrender.com
---
+++ part3/Exercise3_13/index.js
@@ -0,0 +1,71 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan');
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const Person = require('./models/person')
+
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
+
+morgan.token('postData', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body);
+  }
+  return '';
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'));
+
+
+  app.get('/', (request, response) => {
+    Person.find({}).then(persons => {
+      response.json(persons)
+    })
+  })
+  
+  app.get('/api/persons', (request, response) => {
+    Person.find({}).then(persons => {
+      response.json(persons)
+    })
+  })
+
+  app.get('/api/persons/:id', (request, response) => {
+    Person.findById(request.params.id).then(person => {
+      response.json(person)
+    })
+  })
+  
+  /*app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body.name || !body.number) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+
+    const person = new Person({
+      name: body.name,
+      number: body.number,
+    })
+  
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    }).catch(error => {
+      console.log(error)
+      response.status(500).end()
+    })
+  })*/
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
