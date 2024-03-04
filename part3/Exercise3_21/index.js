fa317f8f427f6997872b8f37a086cac69a10823d
Miguelhc95 <miguelhc95@usal.es>
2024-03-04 16:43:17 +0100

    Add Exercise3_21 to folder

---
+++ part3/Exercise3_21/index.js
@@ -0,0 +1,130 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan');
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const Person = require('./models/person')
+
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
+
+morgan.token('postData', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body);
+  }
+  return '';
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'));
+
+
+  app.get('/', (request, response) => {
+    Person.find({}).then(persons => {
+      response.json(persons)
+    })
+  })
+  
+  app.get('/api/persons', (request, response) => {
+    Person.find({}).then(persons => {
+      response.json(persons)
+    })
+  })
+
+  app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+  })
+  
+  app.post('/api/persons', (request, response, next) => {
+    const body = request.body;
+  
+    if (!body.name || !body.number) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      });
+    }
+  
+    Person.findOne({ name: body.name })
+      .then((person) => {
+        if (person) {
+          person.number = body.number;
+          return person.save();
+
+        } else {
+          const newPerson = new Person({
+            name: body.name,
+            number: body.number,
+          });
+          return newPerson.save();
+        }
+      })
+      .then((savedPerson) => {
+        response.json(savedPerson);
+      })
+      .catch((error) => next(error));
+  });
+  
+
+  app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+  })
+
+  app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body;
+  
+    const personToUpdate = {
+      name: body.name,
+      number: body.number
+    };
+  
+    Person.findByIdAndUpdate(request.params.id, personToUpdate, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson);
+      })
+      .catch(error => next(error));
+  });
+  
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  app.use(unknownEndpoint)
+  
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+    
+    if (error.name === 'LenghtError') {
+      return response.status(400).send({ error: 'short lenght of field' })
+    } 
+  
+    next(error)
+  }
+  
+  app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
