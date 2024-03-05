44558a5825fa77e25e496dd369f56f57deae49dc
Miguelhc95 <miguelhc95@usal.es>
2024-03-05 18:59:20 +0100

    Add Exercise4_6 to folder

---
+++ part4/Exercise4_6/utils/list_helper.js
@@ -0,0 +1,44 @@
+const lodash = require('lodash')
+
+const dummy = (blogs) => {
+    return 1
+  }
+
+  const totalLikes = (blogs) => {
+    return blogs.reduce((total, blog) => total + blog.likes, 0);
+  }
+
+  const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return null;
+  
+    let favorite = blogs[0];
+    for (let i = 1; i < blogs.length; i++) {
+      if (blogs[i].likes > favorite.likes) {
+        favorite = blogs[i];
+      }
+    }
+    return {
+      title: favorite.title,
+      author: favorite.author,
+      likes: favorite.likes
+    };
+  }
+
+  const mostBlogs = (blogs) => {
+    if (blogs.length === 0) return null;
+  
+    const authorCounts = lodash.countBy(blogs, 'author');
+    const mostBlogsAuthor = lodash.maxBy(lodash.keys(authorCounts), (author) => authorCounts[author]);
+  
+    return {
+      author: mostBlogsAuthor,
+      blogs: authorCounts[mostBlogsAuthor]
+    };
+  }
+  
+  module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog,
+    mostBlogs
+  }
