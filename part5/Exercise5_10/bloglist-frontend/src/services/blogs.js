37a5d8399c85c5595bf48a89133637736e9ece89
Miguelhc95 <miguelhc95@usal.es>
2024-03-22 19:17:37 +0100

    Add Exercise5_10 to folder

---
+++ part5/Exercise5_10/bloglist-frontend/src/services/blogs.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+const baseUrl = '/api/blogs'
+
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const postBlog = async (data) => {
+  const config = {
+    headers: { Authorization: token }
+  }
+  const response = await axios.post(baseUrl, data, config)
+  return response.data
+}
+
+const putBlog = async (id, data) => {
+  const config = {
+    headers: { Authorization: token }
+  }
+  const response = await axios.put(`${baseUrl}/${id}`, data, config)
+  return response.data
+}
+
+
+export default { getAll, setToken, postBlog, putBlog }
