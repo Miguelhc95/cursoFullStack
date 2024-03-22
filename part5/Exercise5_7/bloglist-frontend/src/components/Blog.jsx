bf073d5c78a60039d2d7b9e73797c88eba6fe468
Miguelhc95 <miguelhc95@usal.es>
2024-03-22 18:20:34 +0100

    Add Exercise5_7 to folder

---
+++ part5/Exercise5_7/bloglist-frontend/src/components/Blog.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  const [formVisible, setFormVisible] = useState(false); 
+
+  const visibleBlog = () => {
+    setFormVisible(!formVisible)
+  }
+
+  const functionLikes = () => {
+    console.log("liked")
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}<button onClick={visibleBlog}>show</button>
+        {formVisible && (<div>
+          <p>
+            {blog.author}
+          </p> 
+          <p>
+            {blog.likes} 
+            <button onClick={functionLikes}>like</button>
+          </p> 
+          <p>
+            {blog.url}
+          </p> 
+          <button onClick={visibleBlog}>hide</button>
+        </div>
+        )}
+      </div>
+  </div>
+)}
+
+export default Blog
