f3378ae20a716f89765aad2b13655811a2d52fb0
Miguelhc95 <miguelhc95@usal.es>
2024-04-12 17:56:37 +0200

    Add Exercise5_19 to folder

---
+++ part5/Exercise5_19/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -0,0 +1,48 @@
+describe('Blog app', function() {
+
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.visit('http://localhost:5173')
+  })
+
+  it('Login form is shown', function() {
+    cy.contains('Log in')
+    cy.contains('application')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('rooteo')
+      cy.get('#password').type('123456')
+      cy.get('#logButton').click()
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('rooteo')
+      cy.get('#password').type('1234567')
+      cy.get('#logButton').click()
+    })
+  })
+
+  describe('A blog can be created', function() {
+      beforeEach(function() {
+        it('user can login 2', function () {
+          cy.get('#username').type('rooteo')
+          cy.get('#password').type('123456')
+          cy.get('#logButton').click()
+        })
+      })
+
+      it('create new blog', function() {
+        cy.get('#username').type('rooteo')
+        cy.get('#password').type('123456')
+        cy.get('#logButton').click()
+        cy.contains('Add blog').click()
+        cy.get('#titleBlog').type('a blog title created by cypress')
+        cy.get('#authorBlog').type('a blog author created by cypress')
+        cy.get('#urlBlog').type('a blog url created by cypress')
+        cy.contains('create').click()    
+      })
+  })
+
+})
