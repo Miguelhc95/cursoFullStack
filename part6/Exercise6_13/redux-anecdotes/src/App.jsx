f9fe118eec41a4d1169fb2c6b8782df10a33400b
Miguelhc95 <miguelhc95@usal.es>
2024-05-07 20:26:35 +0200

    Add Exercise6_12 to folder

---
+++ part6/Exercise6_13/redux-anecdotes/src/App.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import NewAnecdote from './components/AnecdoteForm'
+import Anecdotes from './components/AnecdoteList'
+
+const App = () => {
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <Anecdotes />
+      <h2>create new</h2>
+      <NewAnecdote />
+    </div>
+  )
+}
+
+export default App
---
+++ part6/Exercise6_13/redux-anecdotes/src/main.jsx
@@ -0,0 +1,22 @@
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import App from './App'
+import anecdotesReducer from './reducers/anecdoteReducer'
+import filterReducer from './reducers/filterReducer'
+import { configureStore } from '@reduxjs/toolkit'
+import notificationReducer from './reducers/notificationReducer'
+
+const store = configureStore({
+  reducer: {    
+    anecdotes:  anecdotesReducer,
+    filter: filterReducer,
+    notification: notificationReducer
+  }
+})
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
